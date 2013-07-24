6788c32c9da6b0bd96ee3c3046add3944c43e1d2
unknown <ahogue@SD-HGQJGX1.rlicorp.com>
2013-07-24 10:15:10 -0500

    removed big blobs, using splunk host

--- _templates/splunk/tools/chocolateyInstall.ps1
+++ _templates/splunk/tools/chocolateyInstall.ps1
@@ -1,14 +1,8 @@
 ﻿$packageName = 'splunk' 
 $installerType = 'msi' 
-$sourcePath = 'splunk-5.0.3-163460-x86-release.msi'
-$sourcePath64 = 'splunk-5.0.3-163460-x64-release.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0,1)
-
-$mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
-#Write-Host "Source path $mydir"
-$url = ($mydir | Split-Path | Join-Path -ChildPath "x86" | Join-Path -ChildPath "$sourcePath")
-$url64 = ($mydir | Split-Path | Join-Path -ChildPath "x64" | Join-Path -ChildPath "$sourcePath64")
-#Write-Host "Installing from $url"
+$url = "http://download.splunk.com/releases/5.0.3/splunk/windows/splunk-5.0.3-163460-x86-release.msi"
+$url64 = "http://download.splunk.com/releases/5.0.3/splunk/windows/splunk-5.0.3-163460-x64-release.msi"
 
 Install-ChocolateyPackage "$packageName" "$installerType"  "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes    
