51a6d9f9d412164762ea0fdf78220c684e03068c
unknown <ahogue@SD-HGQJGX1.rlicorp.com>
2013-07-24 11:18:54 -0500

    image added

---
+++ _templates/pdfill/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'pdfill' 
+$installerType = 'exe' 
+$sourcePath = 'PDFill_Beta.exe'
+$silentArgs = '/quiet'
+$validExitCodes = @(0,1)
+
+$mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+#Write-Host "Source path $mydir"
+$url = ($mydir | Split-Path | Join-Path -ChildPath "bin" | Join-Path -ChildPath "$sourcePath")
+#Write-Host "Installing from $url"
+
+Install-ChocolateyPackage "$packageName" "$installerType"  "$silentArgs" "$url" -validExitCodes $validExitCodes    
