23dbbc3944cca7e07631f17801f29efe15bec1a5
unknown <ahogue@SD-HGQJGX1.rlicorp.com>
2013-07-24 23:04:24 -0500

    added preview handler

---
+++ _templates/wordcontentcontroltoolkit/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'wordcontentcontroltoolkit' 
+$installerType = 'msi' 
+$sourcePath = 'setup.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0,1)
+
+$mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+#Write-Host "Source path $mydir"
+$url = ($mydir | Split-Path | Join-Path -ChildPath "content" | Join-Path -ChildPath "$sourcePath")
+#Write-Host "Installing from $url"
+
+Install-ChocolateyPackage "$packageName" "$installerType"  "$silentArgs" "$url" -validExitCodes $validExitCodes    
